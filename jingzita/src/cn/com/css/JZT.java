2ce06cd7e405c15bf86d1068dc2eb7dca41189c7
892621981 <32897340+892621981@users.noreply.github.com>
2017-11-23 15:54:36 +0800

    提交

--- jingzita/src/cn/com/css/JZT.java
+++ jingzita/src/cn/com/css/JZT.java
@@ -7,6 +7,25 @@ public class JZT {
 	
 	public static void function1(){
 		for (int i = 1; i <= 9; i+=2) {
+			for (int k = 1; k <= 9; k+=2) {
+				if(i < k){
+					System.out.print(" ");
+				}
+			}
+			for (int j = 1; j <= 9; j++) {
+				if(i >= j){
+					System.out.print("*");
+				}
+			}
+			System.out.println();
+		}
+		//ÏÂ°ë²¿·Ö
+		for (int i = 7; i >= 1; i-=2) {
+			for (int k = 1; k <= 9; k+=2) {
+				if(i < k){
+					System.out.print(" ");
+				}
+			}
 			for (int j = 1; j <= 9; j++) {
 				if(i >= j){
 					System.out.print("*");
---
+++ jingzita/src/cn/com/css/SBX.java
@@ -0,0 +1,45 @@
+package cn.com.css;
+
+import java.util.Scanner;
+
+public class SBX {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("请输入个数:");
+		int i = sc.nextInt();
+		function1(i);
+	}
+	
+	public static void function1(int n){
+		if(n%2 == 0){
+			n = n-1;
+		}
+		for (int i = 1; i <= n; i+=2) {
+			for (int k = 1; k <= n; k+=2) {
+				if(i < k){
+					System.out.print(" ");
+				}
+			}
+			for (int j = 1; j <= n; j++) {
+				if(i >= j){
+					System.out.print("*");
+				}
+			}
+			System.out.println();
+		}
+		//下半部分
+		for (int i = n-2; i >= 1; i-=2) {
+			for (int k = 1; k <= n; k+=2) {
+				if(i < k){
+					System.out.print(" ");
+				}
+			}
+			for (int j = 1; j <= n; j++) {
+				if(i >= j){
+					System.out.print("*");
+				}
+			}
+			System.out.println();
+		}
+	}
+}
