5d26c72db829af9b163e6fdf6007a000b76cc8bf
892621981 <15902799047@163.com>
2017-11-30 10:02:02 +0800

    ss

--- jingzita/src/cn/com/css/xxx.java
+++ jingzita/src/cn/com/css/xxx.java
@@ -2,6 +2,10 @@ package cn.com.css;
 
 public class xxx {
 	public static void main(String[] args) {
-		System.out.println("hello");
+		System.out.println("nice to meet you");
+	}
+	
+	public static void function(){
+		
 	}
 }
